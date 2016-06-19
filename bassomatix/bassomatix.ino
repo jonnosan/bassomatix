7da73adc7ad03bbdbd0119ca89b1c3d344267ffb
Jonno Downes <jonnosan@gmail.com>
2016-06-19 21:00:11 +1000

    revamped riff algorithm

--- bassomatix/bassomatix.ino
+++ bassomatix/bassomatix.ino
@@ -36,7 +36,11 @@ AltSoftSerial midiSerial;
 #define DENSITUDE_POT A3
 #define WIGGLENESS_POT A1
 #define SCALE_POT A4
-#define PATTERN_POT A5
+#define RIFFLENGTH_POT A5
+
+#define STEPS_PER_BAR 16
+#define MAX_STEPS_PER_RIFF (8*STEPS_PER_BAR)
+
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -54,17 +58,21 @@ void setup() {
 }
 
 
+byte riff[MAX_STEPS_PER_RIFF];
 byte tick = 0;
-byte  beat = 0;
+byte  beat_in_bar = 0;
+byte  beat_in_riff = 0;
+byte bars_in_riff=4;
 int  bar = 0;
-byte ticks_this_note = 0;
-byte note_ptr;
-byte current_note;
+byte last_note=0;
 
 bool playing = false;
 
 byte last_sync_mode = SYNC_EXT;
 byte sync_mode;
+
+#define BASS_CHANNEL 1
+
 
 
 //called for each byte received on MIDI IN
@@ -91,13 +99,6 @@ void process_midi_byte(byte b) {
 }
 
 
-#define MELODY_LENGTH_BARS  12
-#define BASS_CHANNEL 1
-//
-//#define SHIFT_AMOUNT 24
-const byte SHIFT_DURATIONS[] = {6, 12, 24, 48};	//#16th, 8th, quarter, half note
-
-
 const byte SCALE[4][16] = {
   {0, 3, 5, 7, 10, 12, 15, 0, 3, 5, 17, 0, 19, 3, 22, 24}, //minor pentatonic
   {0, 4, 7, 7, 10, 12, 16, 0, 7, 5, 19, 0, 19, 7, 19, 24}, //major pentatonic
@@ -106,20 +107,9 @@ const byte SCALE[4][16] = {
 };
 
 
-const byte DURATIONS[] = {24, 12, 12, 6, 6, 6, 6, 6};
 
 #define ROOT_NOTE 48
-#define CLICKS_PER_BAR (24*4)
-#define TUNE_SIZE 32
-
-byte melody[TUNE_SIZE];
-byte duration[TUNE_SIZE];
-
-byte temp_melody[TUNE_SIZE];
-byte temp_duration[TUNE_SIZE];
-
-//densitude= how often a note is playing : 0 (silent) to 255 (always note playing)
-//wiggleness= variability of notes : 0= all notes the same, 8 = notes from 1 scale, 15=notes from 2 scales
+//#define CLICKS_PER_BAR (24*4)
 
 byte read_scale_number() {
   int raw_scale_pot = map(analogRead(SCALE_POT), 0, 1023, 0, 255);
@@ -136,121 +126,71 @@ byte read_scale_number() {
   }
 }
 
-byte read_pattern_number() {
-  int raw_pattern_pot = map(analogRead(PATTERN_POT), 0, 1023, 0, 255);
-  if (raw_pattern_pot > 220) {
-    return 0;
-  } else if (raw_pattern_pot > 180) {
+byte read_riff_length() {
+  int raw_RIFFLENGTH_POT = map(analogRead(RIFFLENGTH_POT), 0, 1023, 0, 255);
+  if (raw_RIFFLENGTH_POT > 220) {
     return 1;
-  } else if (raw_pattern_pot > 110) {
+  } else if (raw_RIFFLENGTH_POT > 180) {
     return 2;
-  } else if (raw_pattern_pot > 55) {
+  } else if (raw_RIFFLENGTH_POT > 110) {
     return 3;
+  } else if (raw_RIFFLENGTH_POT > 55) {
+    return 4;
   } else  {
-    return 4; //FIXME: this should be a user scale
+    return 8; //FIXME: this should be a user scale
   }
 }
 
 
 byte read_densitude() {
-  return map(analogRead(DENSITUDE_POT), 0, 1023, 0, 255);
+  return map(analogRead(DENSITUDE_POT), 0, 1023, 0, 15);
 }
 byte read_wiggleness() {
-  return map(analogRead(WIGGLENESS_POT), 0, 1023, 0, 15);
-}
-
-void generate_melody(byte root_note) {
-
-
-  byte densitude = read_densitude();
-  byte wiggleness = read_wiggleness();
-  byte scale_number = read_scale_number();
-  byte pattern_number=read_pattern_number();
-
-  Serial.print("PATTERN ");
-  Serial.print(pattern_number);
-  Serial.print("SCALE ");
-  Serial.print(scale_number);
-  Serial.print("ROOT NOTE");
-  Serial.print(root_note);
-  Serial.print("DENSITUDE ");
-  Serial.print(densitude);
-  Serial.print("WIGGLENESS ");
-  Serial.print(wiggleness);
-  Serial.println("");
-
-  byte this_duration;
-  byte this_note;
-  byte total_duration = 0;
-  int i;
-  for (i = 0; total_duration < CLICKS_PER_BAR; i++) {
-    //chose a duration:
-    byte this_duration = DURATIONS[random(densitude >> 5)];
-    duration[i] = this_duration;
-    total_duration += this_duration;
-    //chose a note
-    if (random(densitude + 127) < 127) {
-      this_note = 0;
-    } else {
-      this_note = root_note + SCALE[scale_number][random(wiggleness)];
-    }
-    melody[i] = this_note;
-
-    Serial.print("I=");
-    Serial.print(i);
-    Serial.print(" T=");
-    Serial.print(total_duration);
-    Serial.print(" M=");
-    Serial.print(melody[i]);
-    Serial.print(" D=");
-    Serial.println(duration[i]);
-
-  }
-
-  for (; i < TUNE_SIZE; i++) {
-    melody[i] = 0;
-    duration[i] = 0;
-  }
-
-  ticks_this_note = 0;
-  note_ptr = 0;
+  return map(analogRead(WIGGLENESS_POT), 0, 1023, 1, 15);
 }
 
 
-void mutate_melody() {
-  byte mutation_probability = 50;
-  byte densitude = read_densitude();
-  byte wiggleness = read_wiggleness();
-  byte scale_number = read_scale_number();
+void generate_riff() {
   
-  int i;
-  for ( i = 0; i < TUNE_SIZE; i++) {
-    temp_melody[i] = melody[i];
-    temp_duration[i] = duration[i];
+  
+  byte dens=read_densitude();
+  byte wiggle=read_wiggleness();
+  byte scale=read_scale_number();
+  byte i = dens%4;
+  bars_in_riff=read_riff_length();
+ int steps_in_riff=bars_in_riff*STEPS_PER_BAR;
+
+  Serial.print("BAR: ");      
+  Serial.print( bar);
+  Serial.print(" OF ");      
+  Serial.print( bars_in_riff);
+
+
+
+  byte pulses=bars_in_riff*(1+(dens>>1));
+
+  for (i = 0; i < (MAX_STEPS_PER_RIFF); i++) {
+    riff[i] = 0;
   }
 
+  i = 0;
 
-  byte min_note_length = 6;
-  int j = 0;
-  for (i = 0; i < TUNE_SIZE; i++) {
+  byte pauses  = steps_in_riff - pulses;
+  if (pulses >= 0) {
+    int per_pulse = (pauses / pulses);
+    int remainder = pauses % pulses;
 
-    if ((random(100) <= mutation_probability) && (duration[i] > min_note_length)) {
-      //split this note in two
-      byte duration_1 = temp_duration[i] >> 2;
-      byte duration_2 = temp_duration[i] - duration_1;
-
-      melody[j] = temp_melody[i];
-      duration[j] = duration_1;
-      melody[j + 1] = ROOT_NOTE + SCALE[scale_number][random(wiggleness)];;
-      duration[j + 1] = duration_2;
-      j += 2;
-    } else {
-      melody[j] = temp_melody[i];
-      duration[j] = temp_duration[i];
-      j++;
+    for (byte pulse = 0; pulse < pulses; pulse++) {
+      riff[i % steps_in_riff] = ROOT_NOTE+SCALE[scale][pulse%wiggle];
+      i += 1;
+      i += per_pulse;
+      if (pulse < remainder) {
+        i++;
+      }
     }
-
   }
+  
+  
 }
 void start_playing() {
   Serial.println("start");
@@ -258,12 +198,13 @@ void start_playing() {
 
   playing = true;
   tick = 0;
-  beat = 0;
+  beat_in_bar = 0;
+  beat_in_riff = 0;  
   bar = 0;
 
 
 
-  generate_melody(ROOT_NOTE) ;
+  generate_riff() ;
 
 }
 
@@ -276,67 +217,39 @@ void stop_playing() {
 
 void do_one_tick() {
   midiSerial.write(0xF8);      //send MIDI CLOCK TICK
-  tick++;
-  if (tick == 24) {
+  if (tick%6 == 0) {
     tick = 0;
+    
+    if(last_note>0) {
+      midiSerial.write(0x8F + BASS_CHANNEL); //'PLAY'
+      midiSerial.write(last_note);
+      midiSerial.write(byte(0));//velocity
+    }
 
-    beat++;
-    if (beat == 4) {
+
+    if(riff[beat_in_riff]>0) {
+      last_note=riff[beat_in_riff];
+      midiSerial.write(0x8F + BASS_CHANNEL); //'PLAY'
+      midiSerial.write(last_note);
+      midiSerial.write(127);//velocity
+    }
+
+    beat_in_bar++;
+    beat_in_riff++;
+    
+    if (beat_in_bar == STEPS_PER_BAR) {
+      
       bar++;
-      beat = 0;
-      ticks_this_note = 0;
-      note_ptr = 0;
-      if (digitalRead(HOLD_RIFF_PIN) == HIGH) {
-        if (bar % 4 == 0) {
-          generate_melody(ROOT_NOTE) ;
-        } else {
-          mutate_melody();
-        }
+      beat_in_bar = 0;      
+      generate_riff();
+      if (bar>=bars_in_riff) {
+        beat_in_riff=0;
+        bar=0;
       }
     }
   };
+  tick++;
 
-  if (tick == 0) {
-
-    Serial.println("TICK 0 ");
-
-  }
-
-  //turn off this note on the tick  before we play the next one
-
-  if (ticks_this_note == 1) {
-
-    //silence last note
-    if (current_note > 0) {
-      midiSerial.write(0x8F + BASS_CHANNEL); //'PLAY'
-      midiSerial.write(current_note);
-      midiSerial.write((byte)0x00);          //velocity 0 means note off
-    }
-  }
-
-  if (ticks_this_note <= 0) {
-    Serial.print("TICK ");
-    Serial.print(tick);
-    Serial.print("NOTE PTR ");
-    Serial.print(note_ptr);
-    Serial.println("");
-
-
-    current_note = melody[note_ptr];
-
-
-    ticks_this_note = duration[note_ptr];
-    Serial.print("TPN:");
-    Serial.println(ticks_this_note);
-    note_ptr++;
-    if (current_note != 0) {
-      //play new note
-      midiSerial.write(0x8F + BASS_CHANNEL); //'PLAY'
-      midiSerial.write(current_note);
-      midiSerial.write(127);//velocity
-    }
-  }
-  ticks_this_note--;
 
 }
 
@@ -351,7 +264,7 @@ void loop() {
   byte bpm = map(analogRead(A0), 0, 1023, 40, 180);
   sync_mode = digitalRead(SYNC_PIN);
 
-  digitalWrite(LED_PIN, tick > 12 ? LOW : HIGH); //set the LED to flash once per quarter note
+  digitalWrite(LED_PIN, beat_in_bar%4 == 0  ? LOW : HIGH); //set the LED to flash once per quarter note
 
   if (sync_mode != last_sync_mode) {
     delay(100); //to debounce
